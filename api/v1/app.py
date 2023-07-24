23f686e8ec46edbe823425e8af4cee02f82639bb
Marquis4 <marquistumey12@gmail.com>
2023-07-24 18:24:18 +0000

    bnb

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python3
-"""
-Module for hbnb API api
-"""
-from flask import Flask, render_template, Blueprint, jsonify
+"""Script that starts a Flask app"""
+from flask import Flask, jsonify
 from flask_cors import CORS
 from models import storage
-import os
 from api.v1.views import app_views
+from os import getenv
+
+
+"""Start Flask"""
 app = Flask(__name__)
-cors = CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
+
+"""Register the blueprint app_views"""
 app.register_blueprint(app_views)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+"""Create the CORS instance to allow IPs"""
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_api(exception):
-    """After query return ends current session"""
-    return storage.close()
+def teardown(exception):
+    """Closes session"""
+    storage.close()
 
 
 @app.errorhandler(404)
-def not_found(exception):
-    """Handles page not found error"""
+def errorhandler(error):
+    """Returns a JSON-formated status code for errors"""
     return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST')
-    port = int(os.getenv('HBNB_API_PORT'))
-
-    """ os.environ['HBNB_API_HOST'] = '0.0.0.0'
-    host = '0.0.0.0'
-    os.environ['HBNB_API_PORT'] = '5000'
-    port = '5000'"""
-
-    app.run(host=host, port=port, threaded=True)
+    API_HOST = getenv("HBNB_API_HOST", "0.0.0.0")
+    API_PORT = getenv("HBNB_API_PORT", 5000)
+    app.run(host=API_HOST, port=API_PORT, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,53 +1,33 @@
 #!/usr/bin/python3
-"""
-Module for hbnb API api
-"""
-from flask import Flask, render_template, Blueprint, jsonify
+"""Flask application that retrieves information"""
 from api.v1.views import app_views
-from models import storage
-app = Flask(__name__)
+from flask import jsonify
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route("/status", strict_slashes=False)
 def status():
-    nominal = {"status": "OK"}
-    return jsonify(nominal)
+    """Returns the app status"""
+    return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats/', strict_slashes=False)
+@app_views.route("/stats", methods=["GET"], strict_slashes=False)
 def stats():
-    stats = {"amenities": storage.count('Amenity'),
-             "cities": storage.count('City'),
-             "places": storage.count('Place'),
-             "reviews": storage.count('Review'),
-             "states": storage.count('State'),
-             "users": storage.count('User')}
-    return jsonify(stats)
-<<<<<<< HEAD
-#!/usr/bin/python3
-"""
-Module for hbnb API api
-"""
-from flask import Flask, render_template, Blueprint, jsonify
-from api.v1.views import app_views
-from models import storage
-app = Flask(__name__)
-
-
-@app_views.route('/status', strict_slashes=False)
-def status():
-    nominal = {"status": "OK"}
-    return jsonify(nominal)
-
-
-@app_views.route('/stats/', strict_slashes=False)
-def stats():
-    stats = {"amenities": storage.count('Amenity'),
-             "cities": storage.count('City'),
-             "places": storage.count('Place'),
-             "reviews": storage.count('Review'),
-             "states": storage.count('State'),
-             "users": storage.count('User')}
-    return jsonify(stats)
-=======
->>>>>>> 53dffed5d59703145aefe29ff0d8904b2c910742
+    """Retrieves the number of objects per each type"""
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    from models import storage
+    import json
+    dic = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    json_dict = json.dumps(dic, indent=2)
+    return json_dict
