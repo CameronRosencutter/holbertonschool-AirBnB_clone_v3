0d62424a282551f412b04f45fbaa1103213785ea
Cameron <120847793+CameronRosencutter@users.noreply.github.com>
2023-07-24 13:38:02 -0500

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -26,13 +26,25 @@ CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(exception):
-    """Closes session"""
+    """This is the summary line
+
+This is the further elaboration of the docstring. Within this section,
+you can elaborate further on details as appropriate for the situation.
+Notice that the summary and the elaboration is separated by a blank new
+line.
+"""
     storage.close()
 
 
 @app.errorhandler(404)
 def errorhandler(error):
-    """Returns a JSON-formated status code for errors"""
+    """This is the summary line
+
+This is the further elaboration of the docstring. Within this section,
+you can elaborate further on details as appropriate for the situation.
+Notice that the summary and the elaboration is separated by a blank new
+line.
+"""
     return jsonify({"error": "Not found"}), 404
 
 
