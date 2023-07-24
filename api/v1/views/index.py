4164ad15939102b3e6117f942be9bdea069743b3
Cameron <120847793+CameronRosencutter@users.noreply.github.com>
2023-07-24 13:08:27 -0500

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
-"""
-Module for hbnb API api
-"""
-from flask import Flask, render_template, Blueprint, jsonify
+'''Contains the index view for the API.'''
+from flask import jsonify
+
 from api.v1.views import app_views
 from models import storage
-app = Flask(__name__)
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-@app_views.route('/status', strict_slashes=False)
-def status():
-    nominal = {"status": "OK"}
-    return jsonify(nominal)
+@app_views.route('/status')
+def get_status():
+    '''Gets the status of the API.
+    '''
+    return jsonify(status='OK')
 
 
-@app_views.route('/stats/', strict_slashes=False)
-def stats():
-    stats = {"amenities": storage.count('Amenity'),
-             "cities": storage.count('City'),
-             "places": storage.count('Place'),
-             "reviews": storage.count('Review'),
-             "states": storage.count('State'),
-             "users": storage.count('User')}
-    return jsonify(stats)
+@app_views.route('/stats')
+def get_stats():
+    '''Gets the number of objects for each type.
+    '''
+    objects = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
+    }
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
