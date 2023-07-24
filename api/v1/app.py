8e78116551fc3a650d5e77ed94094b94bb9bd6dc
Cameron <120847793+CameronRosencutter@users.noreply.github.com>
2023-07-24 13:39:30 -0500

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -52,3 +52,4 @@ if __name__ == "__main__":
     API_HOST = getenv("HBNB_API_HOST", "0.0.0.0")
     API_PORT = getenv("HBNB_API_PORT", 5000)
     app.run(host=API_HOST, port=API_PORT, threaded=True)
+...
